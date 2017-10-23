1f36443e35a81dcb54dc854c41ea6564c48c7000
Michael Fenwick <mickyfen17@aol.com>
2017-10-22 19:23:34 -0600

    delete some low quality panos

---
+++ static_assets/images.js
@@ -0,0 +1,380 @@
+const images = [
+  {
+    public_id: 'Panos/IMG_0028_zgiqdz',
+    type: 'upload',
+    bytes: 707842,
+    width: 3098,
+    height: 1014,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576674/Panos/IMG_0028_zgiqdz.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576674/Panos/IMG_0028_zgiqdz.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Telegraph Hill, San Francisco, California',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0026_snhsag',
+    type: 'upload',
+    bytes: 737534,
+    width: 3254,
+    height: 966,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576674/Panos/IMG_0026_snhsag.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576674/Panos/IMG_0026_snhsag.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Palisades Park, Los Angeles, California',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0025_xuik6b',
+    type: 'upload',
+    bytes: 602520,
+    width: 3720,
+    height: 846,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576673/Panos/IMG_0025_xuik6b.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576673/Panos/IMG_0025_xuik6b.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Golden Gate Bridge, San Francisco, California',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0024_y4lueo',
+    type: 'upload',
+    bytes: 835316,
+    width: 2988,
+    height: 1052,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576676/Panos/IMG_0024_y4lueo.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576676/Panos/IMG_0024_y4lueo.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Jakhu Hill, Shimla, India',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0023_nwl3pi',
+    type: 'upload',
+    bytes: 667279,
+    width: 3022,
+    height: 1040,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576668/Panos/IMG_0023_nwl3pi.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576668/Panos/IMG_0023_nwl3pi.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Mala Strana, Prague, Czech Republic',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0022_vcyl5l',
+    type: 'upload',
+    bytes: 616253,
+    width: 3124,
+    height: 1006,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576670/Panos/IMG_0022_vcyl5l.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576670/Panos/IMG_0022_vcyl5l.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Carmel, California',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0021_lswekm',
+    type: 'upload',
+    bytes: 812529,
+    width: 3380,
+    height: 930,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576673/Panos/IMG_0021_lswekm.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576673/Panos/IMG_0021_lswekm.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Down Town, San Diego, California',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0020_cip5rr',
+    type: 'upload',
+    bytes: 864961,
+    width: 3572,
+    height: 880,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576673/Panos/IMG_0020_cip5rr.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576673/Panos/IMG_0020_cip5rr.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Rothbury, North East England',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0018_ekp4hc',
+    type: 'upload',
+    bytes: 780880,
+    width: 3616,
+    height: 870,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576668/Panos/IMG_0018_ekp4hc.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576668/Panos/IMG_0018_ekp4hc.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Hatu Temple, Narkanda Himachal Pradesh, India',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0017_mpozdy',
+    type: 'upload',
+    bytes: 877677,
+    width: 3032,
+    height: 1036,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576664/Panos/IMG_0017_mpozdy.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576664/Panos/IMG_0017_mpozdy.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Hatu Temple, Narkanda Himachal Pradesh, India',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0016_prgcqp',
+    type: 'upload',
+    bytes: 688831,
+    width: 3050,
+    height: 1030,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576665/Panos/IMG_0016_prgcqp.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576665/Panos/IMG_0016_prgcqp.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Lake Windermere, Lake District, United Kingdom',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0015_furtgh',
+    type: 'upload',
+    bytes: 594835,
+    width: 3032,
+    height: 1036,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576662/Panos/IMG_0015_furtgh.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576662/Panos/IMG_0015_furtgh.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Rooftops of Oxford, United Kingdom',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0014_ncqdg0',
+    type: 'upload',
+    bytes: 740271,
+    width: 2954,
+    height: 1064,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576663/Panos/IMG_0014_ncqdg0.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576663/Panos/IMG_0014_ncqdg0.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Bellagio Fountain, Las Vegas, Nevada',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0013_lhvgh5',
+    type: 'upload',
+    bytes: 650582,
+    width: 3354,
+    height: 938,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576658/Panos/IMG_0013_lhvgh5.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576658/Panos/IMG_0013_lhvgh5.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Down Town South Beach, Miami, Florida',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0012_bhhnm5',
+    type: 'upload',
+    bytes: 835063,
+    width: 3252,
+    height: 966,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576657/Panos/IMG_0012_bhhnm5.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576657/Panos/IMG_0012_bhhnm5.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Lombard Street Street, San Francisco, California',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0011_sgtrhf',
+    type: 'upload',
+    bytes: 1634722,
+    width: 5401,
+    height: 1264,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576663/Panos/IMG_0011_sgtrhf.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576663/Panos/IMG_0011_sgtrhf.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Pier 39, San Francisco, California',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0010_accmfm',
+    type: 'upload',
+    bytes: 760762,
+    width: 3372,
+    height: 932,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576648/Panos/IMG_0010_accmfm.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576648/Panos/IMG_0010_accmfm.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Pill Box hike, Lanikai, Hawaii',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0009_mcqsi9',
+    type: 'upload',
+    bytes: 1616513,
+    width: 3624,
+    height: 1300,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576650/Panos/IMG_0009_mcqsi9.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576650/Panos/IMG_0009_mcqsi9.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Big Sur, California',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0008_ceqepn',
+    type: 'upload',
+    bytes: 2800780,
+    width: 4687,
+    height: 1536,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576657/Panos/IMG_0008_ceqepn.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576657/Panos/IMG_0008_ceqepn.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Crater Lake, Colorado',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0007_fptwca',
+    type: 'upload',
+    bytes: 2756916,
+    width: 4659,
+    height: 1536,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576664/Panos/IMG_0007_fptwca.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576664/Panos/IMG_0007_fptwca.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        caption: 'Crater Lakes Trail, Colordao',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0006_ylzfi7',
+    type: 'upload',
+    bytes: 1283550,
+    width: 5402,
+    height: 1254,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576651/Panos/IMG_0006_ylzfi7.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576651/Panos/IMG_0006_ylzfi7.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        alt: 'View of the surrounding mountains from the top of Pike Peak',
+        caption: 'Pike Peak, Colorado',
+      },
+    },
+  },
+  {
+    public_id: 'Panos/IMG_0005_ulde87',
+    type: 'upload',
+    bytes: 2817967,
+    width: 5096,
+    height: 1536,
+    url:
+      'http://res.cloudinary.com/mickyfen17/image/upload/v1507576658/Panos/IMG_0005_ulde87.jpg',
+    secure_url:
+      'https://res.cloudinary.com/mickyfen17/image/upload/v1507576658/Panos/IMG_0005_ulde87.jpg',
+    tags: ['panorama'],
+    context: {
+      custom: {
+        alt: 'Image of Crater Lake and the surrounding mountains',
+        caption: 'Crater Lake, Colorado',
+      },
+    },
+  },
+];
+
+export default images;
